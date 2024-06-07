07d76764f3ddd5aa547031b11af3ba5b046d2363
PedrOliveiraM <pedropucmont@gmail.com>
2024-06-07 15:34:32 -0300

    feat: fix interface tables

--- futdev.py
+++ futdev.py
@@ -9,34 +9,35 @@ def atualizarDF():
     return qtd_jogadores, lista_jogadores
 
 st.set_page_config(page_title='Gerador de Times', page_icon='icone-fut.png')
-st.image('./background.png', width=700 )
+st.image('./background.png', width=700)
 st.title('Gerador de Times')
-
-# Variáveis
-qtd_jogadores = 0   
-lista_jogadores = []
-
-# Campo para indicar a quantidade de jogadores por time
-qtd_por_time = st.number_input('Quantidade de jogadores por time:', min_value=1, step=1)
-
-st.markdown("##### QUADRO DOS JOGADORES") 
-
-columns = ["JOGADORES","CAPITÃO"]
 
 # Inicializa o dataframe vazio se não estiver no estado da sessão
 if 'df_tabela' not in st.session_state:
-    st.session_state.df_tabela = pd.DataFrame(columns=columns)
+    st.session_state.df_tabela = pd.DataFrame(columns=["JOGADORES", "CAPITÃO"])
+
+st.markdown("##### LISTA DE PARTICIPANTES")
+
+txt = st.text_area("Lista dos jogadores")
+
+if st.button('Adicionar Jogadores'):
+    lista_jogadores = txt.split("\n")
+    lista_jogadores = [jogador for jogador in lista_jogadores if jogador.strip() != ""]  # Remove entradas vazias
+    df_tabela = pd.DataFrame([[jogador, False] for jogador in lista_jogadores], columns=["JOGADORES", "CAPITÃO"])
+    st.session_state.df_tabela = df_tabela
+
+st.markdown("##### QUADRO DOS JOGADORES")
 
 # Editor de dados
 df_tabela = st.data_editor(
-    st.session_state.df_tabela, 
+    st.session_state.df_tabela,
     column_config={
         "CAPITÃO": st.column_config.CheckboxColumn(
             default=False,
         )
-    }, 
+    },
     hide_index=True,
-    num_rows='dynamic', 
+    num_rows='dynamic',
     use_container_width=True,
     on_change=atualizarDF
 )
@@ -45,23 +46,27 @@ def generate_teams(df_tabela, qtd_por_time):
     teams = []
     lista_jogadores = df_tabela.values.tolist()
     random.shuffle(lista_jogadores)
-    captains = [jogador[0] for jogador in lista_jogadores if jogador[1] == True] # Lista de capitães
-    n_teams = len(lista_jogadores)//max(1, qtd_por_time)  # Evita a divisão por zero
-    lista_jogadores = [jogador for jogador in lista_jogadores if jogador[1] == False] # Remove os capitães da lista de jogadores
+    captains = [jogador[0] for jogador in lista_jogadores if jogador[1]]  # Lista de capitães
+    lista_jogadores = [jogador[0] for jogador in lista_jogadores if not jogador[1]]  # Remove os capitães da lista de jogadores
     
+    n_teams = max(1, (len(captains) + len(lista_jogadores)) // qtd_por_time)  # Número de times
+
     for i in range(n_teams):
         team = []
+        if captains:  # Adiciona um capitão ao time se disponível
+            team.append(captains.pop(0) + " (Capitão)")
+        
         for j in range(qtd_por_time - 1):  # Adiciona jogadores ao time (quantidade por time - 1)
             if lista_jogadores:
-                player = lista_jogadores.pop(0)[0]
-                team.append(player)
+                team.append(lista_jogadores.pop(0))
             else:
                 break
-        if captains:  # Adiciona um capitão ao final do time
-            team.append(captains.pop(0) + " (Capitão)")
+        
         teams.append(team)
-    
+
     return teams
+
+qtd_por_time = st.number_input('Quantidade de jogadores por time:', min_value=1, step=1)
 
 # Botão para gerar times
 btn_gerar = st.button('Gerar Times')
